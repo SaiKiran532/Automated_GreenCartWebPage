eee5543806a1c52f8f60cd0b78b44281fa3bfd48
Saikiran Challa <164859817+SaiKiran532@users.noreply.github.com>
2024-05-16 13:35:59 +0530

    Update conftest.py

--- Testcases/conftest.py
+++ Testcases/conftest.py
@@ -1,11 +1,18 @@
 import webbrowser
+from selenium.webdriver.chrome.options import Options
 
 from selenium import webdriver
 import pytest
 
 @pytest.fixture()
 def setup():
-    driver = webdriver.Chrome()
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+
+    driver = webdriver.Chrome(options=chrome_options)
     # if browser == 'chrome':
     #     driver = webdriver.Chrome()
     #     print("Launching Chrome browser")
