35327a875870cd9bdbd75988852b4c6bb0de265e
Saikiran Challa <164859817+SaiKiran532@users.noreply.github.com>
2024-05-16 13:19:14 +0530

    Removed browser argument as I getting error.

--- Testcases/conftest.py
+++ Testcases/conftest.py
@@ -4,29 +4,30 @@ from selenium import webdriver
 import pytest
 
 @pytest.fixture()
-def setup(browser):
-    if browser == 'chrome':
-        driver = webdriver.Chrome()
-        print("Launching Chrome browser")
-    elif browser == 'firefox':
-        driver = webdriver.Firefox()
-        print("Launching Firefox browser")
-    elif browser == 'edge':
-        driver = webdriver.Firefox()
-        print("Launching Edge browser")
-    else:
-        driver = webdriver.Chrome()
-        print("Launching default Chrome browser")
+def setup():
+    driver = webdriver.Chrome()
+    # if browser == 'chrome':
+    #     driver = webdriver.Chrome()
+    #     print("Launching Chrome browser")
+    # elif browser == 'firefox':
+    #     driver = webdriver.Firefox()
+    #     print("Launching Firefox browser")
+    # elif browser == 'edge':
+    #     driver = webdriver.Firefox()
+    #     print("Launching Edge browser")
+    # else:
+    #     driver = webdriver.Chrome()
+    #     print("Launching default Chrome browser")
     driver.maximize_window()
     driver.implicitly_wait(5)
     return driver
 
-def pytest_addoption(parser):
-    parser.addoption("--browser")
+# def pytest_addoption(parser):
+#     parser.addoption("--browser")
 
-@pytest.fixture()
-def browser(request):
-    return request.config.getoption("--browser")
+# @pytest.fixture()
+# def browser(request):
+#     return request.config.getoption("--browser")
 
 
 
