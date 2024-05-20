0c4cb30d295b490e035f7a6c9a9234c2d6b1889d
Saikiran Challa <164859817+SaiKiran532@users.noreply.github.com>
2024-05-20 13:49:50 +0530

    New ran

--- Testcases/test_green_cart.py
+++ Testcases/test_green_cart.py
@@ -15,7 +15,7 @@ class TestGreenCart:
     promocode = ReadConfig.get_promo_code()
     logger = LogGen.log_gen()
 
-    scree_shot_path = "Screenshots/test_home_page.png"
+    scree_shot_path = "Screenshots/test_home_page_new.png"
 
     def test_home_page(self, setup):
         self.logger.info("********** GreenCartTest ********** ")
